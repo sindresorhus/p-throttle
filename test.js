88952ba333dd042780af66875a5b54f75ce0a495
Sindre Sorhus <sindresorhus@gmail.com>
2018-12-15 12:27:09 +0100

    Meta tweaks

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import inRange from 'in-range';
 import timeSpan from 'time-span';
 import delay from 'delay';
-import m from '.';
+import pThrottle from '.';
 
 const {Timer} = process.binding('timer_wrap');
 const fixture = Symbol('fixture');
@@ -12,7 +12,7 @@ test('main', async t => {
 	const limit = 5;
 	const interval = 100;
 	const end = timeSpan();
-	const throttled = m(async () => {}, limit, interval);
+	const throttled = pThrottle(async () => {}, limit, interval);
 
 	await Promise.all(new Array(totalRuns).fill(0).map(throttled));
 
@@ -21,7 +21,7 @@ test('main', async t => {
 });
 
 test('passes arguments through', async t => {
-	const throttled = m(async x => x, 1, 100);
+	const throttled = pThrottle(async x => x, 1, 100);
 	t.is(await throttled(fixture), fixture);
 });
 
@@ -29,25 +29,25 @@ test('can be aborted', async t => {
 	const limit = 1;
 	const interval = 10000; // 10 seconds
 	const end = timeSpan();
-	const throttled = m(async () => {}, limit, interval);
+	const throttled = pThrottle(async () => {}, limit, interval);
 
 	await throttled();
-	const p = throttled();
+	const promise = throttled();
 	throttled.abort();
 	let error;
 	try {
-		await p;
+		await promise;
 	} catch (error2) {
 		error = error2;
 	}
-	t.true(error instanceof m.AbortError);
+	t.true(error instanceof pThrottle.AbortError);
 	t.true(end() < 100);
 });
 
 test('exits immediately', async t => {
 	const limit = 2;
 	const interval = 1000;
-	const throttled = m(() => Promise.resolve(), limit, interval);
+	const throttled = pThrottle(() => Promise.resolve(), limit, interval);
 
 	for (let i = 1; i <= 3; i++) {
 		throttled();
@@ -55,10 +55,10 @@ test('exits immediately', async t => {
 
 	await throttled();
 
-	// New tick (because the promise may be finished while the timer *is* pending)
+	// New tick, because the promise may be finished while the timer *is* pending
 	await delay(1);
 	const timers = process._getActiveHandles().filter(handle => {
-		// Check if the handle is a Timer that matches the interval
+		// Check if the handle is a `Timer` that matches the interval
 		return handle instanceof Timer && handle._list.msecs === interval;
 	});
 	t.is(timers.length, 0);
