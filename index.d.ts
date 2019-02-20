c892bc0597a89a7bf4ee752fd1a75e87ce97cb28
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-02-20 05:04:39 +0000

    Improve TypeScript definitions (#13)

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,25 @@
 export class AbortError extends Error {
+	readonly name: 'AbortError';
+
 	/**
 	 * Abort pending execution. All unresolved promised are rejected with a `AbortError` error.
 	 */
 	constructor();
 }
 
+export type ThrottledFunction<Arguments extends unknown[], Return> = ((
+	...arguments: Arguments
+) => Promise<Return>) & {
+	/**
+	 * Abort pending executions. All unresolved promises are rejected with a `pThrottle.AbortError` error.
+	 */
+	abort(): void;
+};
+
 /**
- * Returns a throttled version of `fn`.
+ * [Throttle](https://css-tricks.com/debouncing-throttling-explained-examples/) promise-returning/async/normal functions.
  *
- * @param input - Promise-returning/async function or a normal function.
+ * @param fn - Promise-returning/async function or a normal function.
  * @param limit - Maximum number of calls within an `interval`.
  * @param interval - Timespan for `limit` in milliseconds.
  * @returns A throttled version of `fn`.
@@ -25,4 +36,8 @@ export class AbortError extends Error {
  * 	throttled(i).then(console.log);
  * }
  */
-export default function <T extends Function>(input: T, limit: number, interval: number): T;
+export default function<Arguments extends unknown[], Return>(
+	fn: (...arguments: Arguments) => PromiseLike<Return> | Return,
+	limit: number,
+	interval: number
+): ThrottledFunction<Arguments, Return>;
