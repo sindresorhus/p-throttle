ce1624d1eb31628cf50a08424efa2c40301e06e5
Richie Bendall <richiebendall@gmail.com>
2021-01-20 04:44:29 +1300

    Fix TypeScript typings (#23)

--- index.d.ts
+++ index.d.ts
@@ -7,10 +7,12 @@ declare class AbortErrorClass extends Error {
 	constructor();
 }
 
+type PromiseResolve<ValueType> = ValueType extends PromiseLike<infer ValueType> ? Promise<ValueType> : Promise<ValueType>;
+
 declare namespace pThrottle {
-	type ThrottledFunction<FunctionType extends (...args: any) => any> = ((
-		...arguments: Parameters<FunctionType>
-	) => ReturnType<FunctionType>) & {
+	type ThrottledFunction<Argument, ReturnValue> = ((
+		...arguments: Argument[]
+	) => PromiseResolve<ReturnValue>) & {
 		/**
 		Abort pending executions. All unresolved promises are rejected with a `pThrottle.AbortError` error.
 		*/
@@ -30,11 +32,6 @@ declare namespace pThrottle {
 	}
 
 	type AbortError = AbortErrorClass;
-
-	/**
-	@param fn - Promise-returning/async function or a normal function.
-	*/
-	type Throttle<FunctionType extends (...args: any) => any> = (fn: (...arguments: Parameters<FunctionType>) => ReturnType<FunctionType>) => ThrottledFunction<FunctionType>;
 }
 
 declare const pThrottle: {
@@ -65,9 +62,9 @@ declare const pThrottle: {
 	}
 	```
 	*/
-	<FunctionType extends (...args: any) => any>(
+	(
 		options: pThrottle.Options
-	): pThrottle.Throttle<FunctionType>;
+	): <Argument, ReturnValue>(function_: (...arguments: Argument[]) => ReturnValue) => pThrottle.ThrottledFunction<Argument, ReturnValue>;
 };
 
 export = pThrottle;
