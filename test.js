5cb0a4e5ade05c8d06ad4438b702ef2664ffb013
Richie Bendall <richiebendall@gmail.com>
2021-01-19 22:00:14 +1300

    Require Node.js 10 and currify function (#22)

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
-/* eslint-disable node/no-deprecated-api */
 import test from 'ava';
 import inRange from 'in-range';
 import timeSpan from 'time-span';
-import delay from 'delay';
 import pThrottle from '.';
 
-const {Timer} = process.binding('timer_wrap');
 const fixture = Symbol('fixture');
 
 test('main', async t => {
@@ -13,16 +10,16 @@ test('main', async t => {
 	const limit = 5;
 	const interval = 100;
 	const end = timeSpan();
-	const throttled = pThrottle(async () => {}, limit, interval);
+	const throttled = pThrottle({limit, interval})(async () => {});
 
 	await Promise.all(new Array(totalRuns).fill(0).map(throttled));
 
 	const totalTime = (totalRuns * interval) / limit;
-	t.true(inRange(end(), totalTime - 100, totalTime + 100));
+	t.true(inRange(end(), totalTime - 200, totalTime + 200));
 });
 
 test('passes arguments through', async t => {
-	const throttled = pThrottle(async x => x, 1, 100);
+	const throttled = pThrottle({limit: 1, interval: 100})(async x => x);
 	t.is(await throttled(fixture), fixture);
 });
 
@@ -30,7 +27,7 @@ test('can be aborted', async t => {
 	const limit = 1;
 	const interval = 10000; // 10 seconds
 	const end = timeSpan();
-	const throttled = pThrottle(async () => {}, limit, interval);
+	const throttled = pThrottle({limit, interval})(async () => {});
 
 	await throttled();
 	const promise = throttled();
@@ -44,24 +41,4 @@ test('can be aborted', async t => {
 
 	t.true(error instanceof pThrottle.AbortError);
 	t.true(end() < 100);
-});
-
-test('exits immediately', async t => {
-	const limit = 2;
-	const interval = 1000;
-	const throttled = pThrottle(() => Promise.resolve(), limit, interval);
-
-	for (let i = 1; i <= 3; i++) {
-		throttled();
-	}
-
-	await throttled();
-
-	// New tick, because the promise may be finished while the timer *is* pending
-	await delay(1);
-	const timers = process._getActiveHandles().filter(handle => {
-		// Check if the handle is a `Timer` that matches the interval
-		return handle instanceof Timer && handle._list.msecs === interval;
-	});
-	t.is(timers.length, 0);
 });
